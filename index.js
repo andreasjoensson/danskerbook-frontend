40e45e2427104590489aab40010b95737ca377be
Andreas <andreas.ecuador@live.dk>
2021-09-14 21:04:07 +0200

    donedone

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-if(process.env.NODE_ENV !== "production"){
+if(process.env.NODE_ENV === "production"){
     app.use(express.static("build"));
     app.get('*', (req,res) => {
       req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
